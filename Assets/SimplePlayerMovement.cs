e764febbded74c7934897d21f23d34c874ed5017
Marc Fernández <marcausente@gmail.com>
2025-04-21 18:15:25 +0200

    FEAT: Implemented script for player movement

--- Assets/SimplePlayerMovement.cs
+++ Assets/SimplePlayerMovement.cs
@@ -1,6 +1,45 @@
+using System.Collections;
+using System.Collections.Generic;
 using UnityEngine;
+using Photon.Pun;
 
-public class SimplePlayerMovemen
+public class SimplePlayerMovement : MonoBehaviour
 {
-    
+	
+	void Start ()
+	{
+
+	}
+
+	void Update()
+	{
+		float speed = 1f;
+float rspeed = 5f;
+
+if (Input.GetKey(KeyCode.A)){
+	transform.Translate(-speed * Time.deltaTime, 0f, 0f);
 }
+if (Input.GetKey(KeyCode.D)){
+	transform.Translate(speed * Time.deltaTime, 0f, 0f);
+}
+
+if (Input.GetKey(KeyCode.W)){
+	transform.Translate(0f, 0f, speed * Time.deltaTime);
+}
+
+if (Input.GetKey(KeyCode.S)){
+	transform.Translate(0f, 0f, -speed * Time.deltaTime);
+}
+
+if (Input.GetKey(KeyCode.Q)){
+	transform.Rotate(0f, -rspeed * Time.deltaTime, 0f);
+}
+
+if (Input.GetKey(KeyCode.E)){
+	transform.Rotate(0f, rspeed * Time.deltaTime, 0f);
+}
+	}
+
+		
+}
+
