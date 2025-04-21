f6bc5aeae204323d0c2c9fcaf3cf398808f09859
Marc Fernández <marcausente@gmail.com>
2025-04-21 18:23:25 +0200

    FIX: Movimiento de ambos jugadores de forma independiente

--- Assets/SimplePlayerMovement.cs
+++ Assets/SimplePlayerMovement.cs
@@ -3,8 +3,10 @@ using System.Collections.Generic;
 using UnityEngine;
 using Photon.Pun;
 
-public class SimplePlayerMovement : MonoBehaviour
+public class SimplePlayerMovement : MonoBehaviourPunCallbacks
 {
+	float speed = 1f;
+	float rspeed = 5f;
 	
 	void Start ()
 	{
@@ -13,31 +15,31 @@ public class SimplePlayerMovement : MonoBehaviour
 
 	void Update()
 	{
-		float speed = 1f;
-float rspeed = 5f;
+			if(photonView.IsMine){ 
 
-if (Input.GetKey(KeyCode.A)){
-	transform.Translate(-speed * Time.deltaTime, 0f, 0f);
-}
-if (Input.GetKey(KeyCode.D)){
-	transform.Translate(speed * Time.deltaTime, 0f, 0f);
-}
+				if (Input.GetKey(KeyCode.A)){
+					transform.Translate(-speed * Time.deltaTime, 0f, 0f);
+				}
+				if (Input.GetKey(KeyCode.D)){
+					transform.Translate(speed * Time.deltaTime, 0f, 0f);
+				}
 
-if (Input.GetKey(KeyCode.W)){
-	transform.Translate(0f, 0f, speed * Time.deltaTime);
-}
+				if (Input.GetKey(KeyCode.W)){
+					transform.Translate(0f, 0f, speed * Time.deltaTime);
+				}
 
-if (Input.GetKey(KeyCode.S)){
-	transform.Translate(0f, 0f, -speed * Time.deltaTime);
-}
+				if (Input.GetKey(KeyCode.S)){
+					transform.Translate(0f, 0f, -speed * Time.deltaTime);
+				}
 
-if (Input.GetKey(KeyCode.Q)){
-	transform.Rotate(0f, -rspeed * Time.deltaTime, 0f);
-}
+				if (Input.GetKey(KeyCode.Q)){
+					transform.Rotate(0f, -rspeed * Time.deltaTime, 0f);
+				}
 
-if (Input.GetKey(KeyCode.E)){
-	transform.Rotate(0f, rspeed * Time.deltaTime, 0f);
-}
+				if (Input.GetKey(KeyCode.E)){
+					transform.Rotate(0f, rspeed * Time.deltaTime, 0f);
+			}
+		}
 	}
 
 		
